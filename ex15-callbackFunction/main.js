4ce2321c24334441e754fb27a32faee8b62e6f6c
snrtn <snrtn94@gmail.com>
2022-01-14 14:51:13 +0100

    exercice JS

---
+++ ex15-callbackFunction/main.js
@@ -0,0 +1,57 @@
+function calculator(op, num1, num2, callback) {
+    var result = '';
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = 'not find';
+    }
+    callback(result);
+}
+function printf1(result) {
+    document.write(result + '<br/>');
+    // printf2(result, printf3);
+}
+function printf2(result) {
+    // dans parameter (callback)
+    document.write(result + '<br/>');
+    // callback(result);
+}
+function printf3(result) {
+    document.write(result + '<br/>');
+}
+function printf4(result) {
+    document.write(result + '<br/>');
+}
+calculator('+', 500, 100, printf1);
+calculator('-', 500, 100, printf2);
+calculator('*', 500, 100, printf3);
+calculator('/', 500, 100, printf4);
+// la maintenabilité est simplement
+// la lisibilité est facilement
+// normal
+function addbasic(n1, n2) {
+    return n1 + n2;
+}
+var resultbasic = addbasic(200, 300);
+document.write('= ' + resultbasic + '<br/>');
+// callback
+function add(n1, n2, callback) {
+    var temp = n1 + n2;
+    callback(temp);
+    document.write('= ' + temp + '<br/>');
+}
+function result(value) {
+    document.write('= ' + value + '<br/>');
+}
+add(50, 100, result);
